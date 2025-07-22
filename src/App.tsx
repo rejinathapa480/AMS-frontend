ca8a14504a56c5408f8fee93fd6c42cd18bb41c8
rejinathapa480 <rejinathapa707@gmail.com>
2025-07-22 16:05:43 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from './components/ui/button';
+function App() {
+  return (
+    <>
+   <div className='flex flex-col items-center justify-center mt-10'>
+   <Button>click me</Button>
+   </div>
+    </>
+  )
+}
+
+export default App
